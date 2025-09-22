d26cd35d12d3cc628177fd7fe185746268ade98e
Guillaume Viger <fenekku@users.noreply.github.com>
2025-09-22 15:16:08 -0400

    feat(ingest): ingest views or downloads

--- invenio_analytics_importer/ingest.py
+++ invenio_analytics_importer/ingest.py
@@ -13,9 +13,33 @@ from datetime import datetime, timezone
 from invenio_search import current_search_client
 from invenio_search.engine import search
 
-from invenio_analytics_importer.convert import (
-    iter_to_entries_of_download_analytics,
-)
+
+def to_view(entry, cache):
+    """To view."""
+    recid = entry.pid
+    year_month_day = entry.year_month_day
+    event_name = "record-view"
+    year_month = year_month_day.rsplit("-", 1)[0]
+    count = entry.views
+    unique_count = entry.visits
+    parent_recid = cache.get_parent_pid(recid)
+
+    return {
+        "_id": f"ui_{recid}-{year_month_day}",
+        "_index": f"stats-{event_name}-{year_month}",
+        "_source": {
+            # Since those entries are synthetic anyway, we place them at
+            # the start of the day
+            "timestamp": f"{year_month_day}T00:00:00",
+            "unique_id": f"ui_{recid}",
+            "count": count,
+            "updated_timestamp": datetime.now(timezone.utc).isoformat(),
+            "unique_count": unique_count,
+            "recid": recid,
+            "parent_recid": parent_recid,
+            "via_api": False,
+        },
+    }
 
 
 def to_download(entry, cache):
@@ -53,23 +77,17 @@ def to_download(entry, cache):
     }
 
 
-def iter_to_stats_file_download_actions(filepaths, cache):
-    """Iterable of download actions as fully formed input to search engine."""
-    for entry in iter_to_entries_of_download_analytics(filepaths):
-        yield to_download(entry, cache)
+def generate_download_stats(analytics, cache):
+    """Generator for download statistics actions."""
+    for analytic in analytics:
+        yield to_download(analytic, cache)
 
 
-def ingest_downloads(downloads):
-    """Ingest downloads."""
+def ingest_statistics(actions):
+    """Ingest statistics actions."""
     search.helpers.bulk(
         current_search_client,
-        downloads,
+        actions,
         stats_only=True,
         chunk_size=50,
     )
-
-
-def ingest_download_analytics_from_filepaths(filepaths, cache):
-    """Ingest."""
-    downloads = iter_to_stats_file_download_actions(filepaths, cache)
-    ingest_downloads(downloads)
