61f037ee223265d6ef10843376784793ef463931
Guillaume Viger <fenekku@users.noreply.github.com>
2025-09-25 10:36:52 -0400

    feat(ingest-views): ingest views analytics

--- invenio_analytics_importer/convert.py
+++ invenio_analytics_importer/convert.py
@@ -48,3 +48,36 @@ def generate_download_analytics(raw_analytics):
     """Yield DownloadAnalytics entries from raw entries."""
     for year_month_day, raw in raw_analytics:
         yield DownloadAnalytics.create(year_month_day, raw)
+
+
+@dataclasses.dataclass
+class ViewAnalytics:
+    """Intermediate representation of view analytics."""
+
+    year_month_day: str  # keeping it simple for now
+    pid: str
+    visits: int
+    views: int
+
+    @classmethod
+    def create(cls, year_month_day, analytics_raw):
+        """Create from raw analytics."""
+        label = analytics_raw.get("label", "")
+
+        # extract "3s45v-k5m55" from ".../records/3s45v-k5m55[/...]"
+        regex_pid = re.compile(r"/records/([^/]*)(?:/|$)")
+        match = regex_pid.search(label)
+        pid = match.group(1) if match else ""
+
+        return cls(
+            year_month_day=year_month_day,
+            pid=pid,
+            visits=analytics_raw.get("nb_visits", 0),
+            views=analytics_raw.get("nb_hits", 0),
+        )
+
+
+def generate_view_analytics(raw_analytics):
+    """Yield ViewAnalytics entries from raw entries."""
+    for year_month_day, raw in raw_analytics:
+        yield ViewAnalytics.create(year_month_day, raw)
